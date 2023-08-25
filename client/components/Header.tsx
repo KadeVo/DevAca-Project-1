a076e0a8c6e82f2db0e3e7ba8680df6cdd7d2ff8
Matija Miletic <matijamiletic@protonmail.com>
2023-08-25 14:15:50 +1200

    added banner to header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
-import { getCat, likeCatPic } from '../apiClient.ts'; // Remove ".ts" extensions
-import { Cat } from '../../models/models.ts'; // Remove ".ts" extensions
+import { useEffect, useState } from 'react'
+import { getCat, likeCatPic } from '../apiClient.ts' // Remove ".ts" extensions
+import { Cat } from '../../models/models.ts' // Remove ".ts" extensions
 
 function Header() {
-  const [cat, setCat] = useState<Cat[] | null>(null); // Specify the type for useState
+  const [cat, setCat] = useState<Cat[] | null>(null) // Specify the type for useState
 
   useEffect(() => {
     async function fetchCats() {
       try {
-        const newCat = await getCat();
-        console.log(newCat);
-        setCat(newCat);
+        const newCat = await getCat()
+        console.log(newCat)
+        setCat(newCat)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     }
-    fetchCats();
-  }, []);
+    fetchCats()
+  }, [])
 
   return (
     <div className="header">
       <div className="logoContainer">
-        <img src="images/dc65035f868ac2a6882509e856ebc36d.png" alt="Logo" className="logo" />
+        <img src="images/OnlyCatsFinal.png" alt="Logo" className="logo" />
         <h1>OnlyCats</h1>
       </div>
       <div className="randomCat">
@@ -35,14 +35,18 @@ function Header() {
         ))}
       </div>
       <div className="navContainer">
-
-
-        <a href="index.html" className="onlyButton">Back to Home</a>
-        <button type="button" className="onlyButton randomButton">Random Cat</button>
-        <button type="button" className="onlyButton browseButton">Browse OnlyCats</button>
+        <a href="index.html" className="onlyButton">
+          Back to Home
+        </a>
+        <button type="button" className="onlyButton randomButton">
+          Random Cat
+        </button>
+        <button type="button" className="onlyButton browseButton">
+          Browse OnlyCats
+        </button>
       </div>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
